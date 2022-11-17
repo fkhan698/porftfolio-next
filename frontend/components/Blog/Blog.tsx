64dcac352217a1a07ecdaa9a7125b73572079916
Faizan Khan <fkhan698@hotmail.com>
2022-11-17 15:47:10 -0600

    Fix CORS error

--- frontend/components/Blog/Blog.tsx
+++ frontend/components/Blog/Blog.tsx
@@ -1,29 +1,34 @@
-import React from "react"
-import styles from "./Blog.module.scss"
-import { useQuery } from "@apollo/client"
-import gql from "graphql-tag"
-import client from "../../helpers/apollo-client"
+import React from "react";
+import styles from "./Blog.module.scss";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import client from "../../helpers/apollo-client";
 
-import BlogPost from "./BlogPost"
+import BlogPost from "./BlogPost";
 const ALL_POSTS_QUERY = gql`
   query ALL_POSTS {
     blogPosts {
       id
-      name
+      publishedAt
+      title
     }
   }
-`
+`;
 
 const Blog = ({}) => {
-  const { data } = useQuery(ALL_POSTS_QUERY)
-  console.log(data)
+  const { data, error, loading } = useQuery(ALL_POSTS_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  console.log(data);
   return (
     <>
-      <div className={styles.HerroContainer}>
-        <BlogPost />
-      </div>
+      <div className={styles.HerroContainer}></div>
+      {data.blogPosts.map(post => {
+        <h1>{post.title}</h1>;
+        <h1>Jsafas</h1>;
+      })}
     </>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
