7f5cb1210fd9b73647651e4201b2666a2f7dcd29
Faizan Khan <fkhan698@hotmail.com>
2022-11-20 20:37:01 -0600

    Fix styles

--- frontend/components/Blog/Blog.tsx
+++ frontend/components/Blog/Blog.tsx
@@ -1,11 +1,11 @@
-import React from "react"
-import styles from "./Blog.module.scss"
-import { useQuery } from "@apollo/client"
-import gql from "graphql-tag"
-import client from "../../helpers/apollo-client"
-import Link from "next/link"
+import React from "react";
+import styles from "./Blog.module.scss";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import client from "../../helpers/apollo-client";
+import Link from "next/link";
 
-import BlogPost from "./BlogPost"
+import BlogPost from "./BlogPost";
 const ALL_POSTS_QUERY = gql`
   query ALL_POSTS {
     blogPosts {
@@ -14,26 +14,22 @@ const ALL_POSTS_QUERY = gql`
       title
     }
   }
-`
+`;
 
 const Blog = ({}) => {
-  const { data, error, loading } = useQuery(ALL_POSTS_QUERY)
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
-  console.log(data)
+  const { data, error, loading } = useQuery(ALL_POSTS_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  console.log(data);
   return (
     <>
-      <div className={styles.HerroContainer}>
-        {data.blogPosts.map((post) => (
-          <div className={styles.postGrid}>
-            <div className={styles.post}>
-              <BlogPost key={post.id} post={post} />
-            </div>
-          </div>
+      <div className={styles.BlogContainer}>
+        {data.blogPosts.map(post => (
+          <BlogPost key={post.id} post={post} />
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
--- frontend/components/Blog/BlogPost.tsx
+++ frontend/components/Blog/BlogPost.tsx
@@ -1,23 +1,25 @@
-import React from "react"
-import Link from "next/link"
-import styles from "./Blog.module.scss"
+import React from "react";
+import Link from "next/link";
+import styles from "./Blog.module.scss";
 const BlogPost = ({ post }: any) => {
   return (
     <>
       <div className={styles.items}>
-        <Link href="/">
-          <img
-            className={styles.postImage}
-            src="https://images.unsplash.com/photo-1502581827181-9cf3c3ee0106?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=788&q=80"
-            alt=""
-          />
-        </Link>
-        <Link href={`/blogPost/${post.id}`}>
-          <h1 className={styles.postTitle}>{post.title}</h1>
-        </Link>
+        <div className={styles.post}>
+          <Link href="/">
+            <img
+              className={styles.postImage}
+              src="https://images.unsplash.com/photo-1502581827181-9cf3c3ee0106?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=788&q=80"
+              alt=""
+            />
+          </Link>
+          <Link href={`/blogPost/${post.id}`}>
+            <h1 className={styles.postTitle}>{post.title}</h1>
+          </Link>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default BlogPost
+export default BlogPost;
