f77d7549219afee58bd3070c3c586530039eebed
Faizan <fkhan698@hotmail.com>
2022-11-21 18:16:11 -0600

    Add single post data

--- frontend/components/Blog/SinglePost/SinglePost.tsx
+++ frontend/components/Blog/SinglePost/SinglePost.tsx
@@ -1,11 +1,33 @@
 import React from "react"
 import { useQuery } from "@apollo/client"
 import gql from "graphql-tag"
+import {
+  DocumentRenderer,
+  DocumentRendererProps,
+} from "@keystone-6/document-renderer"
+import Header from "../../Header/Header"
 
+import styles from "./SinglePost.module.scss"
+
+const renderers: DocumentRendererProps["renderers"] = {
+  inline: {
+    bold: ({ children }) => {
+      return <strong>{children}</strong>
+    },
+  },
+  block: {
+    paragraph: ({ children, textAlign }) => {
+      return <p>{children}</p>
+    },
+  },
+}
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
     blogPost(where: { id: $id }) {
       title
+      content {
+        document
+      }
     }
   }
 `
@@ -18,12 +40,20 @@ const SinglePost = ({ id }: any) => {
   })
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error</p>
-  const { Post } = data
-  console.log(Post)
+  const { blogPost } = data
+
   return (
-    <div>
-      <h1>Title</h1>
-    </div>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <DocumentRenderer
+            document={blogPost.content.document}
+            renderers={renderers}
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
