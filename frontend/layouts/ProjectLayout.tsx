4672de12b6db04afe3b1b2f3a7fa4604394a8d22
Faizan <fkhan698@hotmail.com>
2023-01-11 12:51:15 -0600

    Add Projects page

---
+++ frontend/layouts/ProjectLayout.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
+import Blog from "../components/Blog/Blog"
+import Projects from "../components/Projects/Projects"
+
+const ProjectsLayout = ({}) => {
+  return (
+    <>
+      <Header />
+      <Projects />
+    </>
+  )
+}
+
+export default ProjectsLayout
