344931f6194b9fbbdc0d1b185d17d255905703e0
Faizan <fkhan698@hotmail.com>
2023-04-05 11:13:31 -0500

    Reconfigure env variables

--- backend/my-project/config/env/production/database.js
+++ backend/my-project/config/env/production/database.js
@@ -1,4 +1,14 @@
 module.exports = ({ env }) => ({
-  url: env("DATABASE_URL"),
-  ssl: env.bool(true),
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 7211),
+      database: env("PGDATABASE", "railway"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "password"),
+      ssl: env.bool(false),
+    },
+    debug: false,
+  },
 });
